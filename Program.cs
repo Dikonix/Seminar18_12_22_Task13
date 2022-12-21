6bbafdb7eba5b925a2305e9120e83f142ae942a8
Diana Brik <di.brik@yandex.kz>
2022-12-21 13:10:46 +0600

    Seminar 18.12.22 Task 13

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+if (number is not null)
+{
+    if (number.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine(number[2]);
+    }
+}
